42860b863ea3da1d06334e7c32258acb9951a7b0
alexmitroff <alexmitroff@gmail.com>
2019-06-16 20:57:53 +0300

    Add vertex class for handle vertexes in obj file

---
+++ formats/obj/vertex.py
@@ -0,0 +1,39 @@
+class Vertex:
+    """Vertex class
+
+    Attrs:
+        `coords` (list/tuple)
+
+    Properties:
+        `x` (float)
+        `y` (float)
+        `z` (float)
+        `w` (float)
+        `as_vector` (tuple)
+
+    Usage:
+        ```
+        from formats.obj.vertex import Vertex
+
+        coords = (1.0, 0.5, 0.7, 1.0)
+        vertex = Vertex(coords)
+        
+        print(vertex.x)
+        print(vertex.as_vertex)
+        ```
+    """
+ 
+    __slots__ = ['x', 'y', 'z', 'w']
+
+    def __init__(self, coords=None):
+        if not coords:
+            coords = (0.0, 0.0, 0.0, 1.0)
+
+        self.x = coords[0]
+        self.y = coords[1]
+        self.z = coords[2]
+        self.w = coords[3]
+
+    @property
+    def as_vector(self):
+        return (self.x, self.y, self.z, self.w)
