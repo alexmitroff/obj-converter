cd0f4218a451978777cb1e6f10ee10fc4e3413cc
alexmitroff <alexmitroff@gmail.com>
2019-06-19 23:39:15 +0300

    Update Vertex class

--- formats/obj/primitives/vertex.py
+++ formats/obj/primitives/vertex.py
@@ -1,4 +1,6 @@
 import numpy as np
+from formats.obj.settings import *
+
 
 class Vertex:
     """Vertex class
@@ -11,21 +13,21 @@ class Vertex:
         `y` (float)
         `z` (float)
         `w` (float)
-        `as_vector` (tuple)
+        `as_vector` (numpy Array)
 
     Usage:
         ```
-        from formats.obj.vertex import Vertex
+        from formats.obj.primitives.vertex import Vertex
 
         coords = (1.0, 0.5, 0.7, 1.0)
         vertex = Vertex(coords)
-        
+
         print(vertex.x)
-        print(vertex.as_vertex)
+        print(vertex.as_vector)
         ```
     """
- 
-    __slots__ = ['x', 'y', 'z', 'w']
+
+    __slots__ = ('x', 'y', 'z', 'w')
 
     def __init__(self, coords=None):
         if not coords:
@@ -34,8 +36,11 @@ class Vertex:
         self.x = coords[0]
         self.y = coords[1]
         self.z = coords[2]
-        self.w = coords[3]
+        if len(coords) == 4:
+            self.w = coords[3]
+        else:
+            self.w = 1.0
 
     @property
     def as_vector(self):
-        return np.array(self.x, self.y, self.z, self.w)
+        return np.array((self.x, self.y, self.z, self.w))
