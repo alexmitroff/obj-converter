b0a3210ef43fe666e6cf2c6e723881a082edbdfc
alexmitroff <alexmitroff@gmail.com>
2019-06-24 09:31:37 +0300

    Add script execution time

--- obj-converter.py
+++ obj-converter.py
@@ -1,5 +1,7 @@
 import argparse
+from datetime import datetime, timedelta
 from pathlib import Path
+
 from formats.obj.model import Model
 
 parser = argparse.ArgumentParser()
@@ -8,6 +10,7 @@ args = parser.parse_args()
 
 
 def main():
+    stamp_start = datetime.now()
     input_file = Path(args.input)
 
     if not input_file.is_file():
@@ -16,6 +19,9 @@ def main():
     with input_file.open() as obj_file:
         model = Model()
         model.read_obj(obj_file)
+    stamp_end = datetime.now()
+    delta = stamp_end - stamp_start
+    print(f'OBJ convertion time: {delta}')
 
 
 if __name__ == '__main__':
